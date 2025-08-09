5697ab0e6377a1c3620306f3658e965558889cd7
crlotwhite <crlotwhite@gmail.com>
2025-08-09 21:03:11 +0900

    ci(windows): fix MSBuild args and ctest config for VS preset (use --parallel and -C Debug); set configuration in presets

--- scripts/build.bat
+++ scripts/build.bat
@@ -3,6 +3,9 @@ setlocal enabledelayedexpansion
 set PRESET=%1
 if "%PRESET%"=="" set PRESET=debug-vs
 
+set CTEST_CONFIG=
+if /I "%PRESET%"=="debug-vs" set CTEST_CONFIG=-C Debug
+
 cmake --preset %PRESET%
-cmake --build --preset %PRESET% -m
-ctest --preset %PRESET% --output-on-failure
+cmake --build --preset %PRESET% --parallel
+ctest --preset %PRESET% %CTEST_CONFIG% --output-on-failure
